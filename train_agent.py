f842b6e57298b6fc93fc6ae442783a47691925ed
Rudimentary_Chef <akrish6@gatech.edu>
2024-02-13 20:38:40 -0500

    adds a basic render option

--- train_agent.py
+++ train_agent.py
@@ -1,5 +1,14 @@
 import gym
 
-env = gym.make("LunarLander-v2")
+env = gym.make("LunarLander-v2", render_mode = "human")
 
 env.reset()
+#prints out a sample action
+for step in range(200):
+    #renders our thing
+    env.render()
+    env.step(env.action_space.sample())
+#closes it
+env.close()
+
+#Changing the hyper parameters will give you about 5-10 percent. Reward mechanism, observation space.
