70158ff154b52610aaa483ad375e312ada4c5e28
Rudimentary_Chef <akrish6@gatech.edu>
2024-02-15 23:30:24 -0500

    adds A2C version

--- train_agent.py
+++ train_agent.py
@@ -1,22 +1,27 @@
 import gym
 from stable_baselines3 import PPO
+from stable_baselines3 import A2C
 import os
-env = gym.make("LunarLander-v2", render_mode = "human")
+env = gym.make("LunarLander-v2", render_mode = 'ansi')
 #day one
 env.reset()
-models_dir = "models/PPO"
+models_dir = "models/A2C"
 logdir = "logs"
 if not os.path.exists(models_dir):
     os.makedirs(models_dir)
 if not os.path.exists(logdir):
     os.makedirs(logdir)
 
-model = PPO("MlpPolicy", env, verbose = 1)
+model = A2C("MlpPolicy", env, verbose = 1)
 TIMESTEPS = 10000
-for i in range(30):
-    model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps= False, tb_log_name= "PPO")
+for i in range(1,30):
+    #By setting it to false we are setting its learning reseting to be false
+    model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps= False, tb_log_name= "A2C")
+    #This will save the model every 10 thousand steps
+    model.save(f"{models_dir}/{TIMESTEPS*i}")
 #prints out a sample action
 episodes = 10
+'''
 for ep in range(episodes):
     obs = env.reset()
     terminated = False
@@ -26,5 +31,5 @@ for ep in range(episodes):
         obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
         #closes it
 env.close()
-
+'''
 #Changing the hyper parameters will give you about 5-10 percent. Reward mechanism, observation space.
